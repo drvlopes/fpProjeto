744bd63db620085a413de7ccd61ae48059df18d5
Diogo Lopes <2211850@my.ipleiria.pt>
2021-11-12 11:12:57 +0000

    teste

---
+++ cmake-build-debug/CMakeFiles/fpProjeto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fpProjeto.dir/main.c.o"
+  "fpProjeto"
+  "fpProjeto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fpProjeto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
